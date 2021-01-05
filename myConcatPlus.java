f6417e354d35a02d1a460bf016ea972088bc431b
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-01-04 16:26:55 -0800

    Concatenate strings with plus sign.

---
+++ myConcatPlus.java
@@ -0,0 +1,9 @@
+
+
+public class myConcatPlus {
+  public static void main(String args[]) {
+    String firstName = "John";
+    String lastName = "Doe";
+    System.out.println(firstName + " " + lastName);
+  }
+}
